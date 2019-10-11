69efe806dabf11ba0bd5e77450dbf6b0f777c4d4
Sergey Tolmachev <tetron@gmail.com>
2019-10-11 21:23:00 +0200

    add links to explanation and implementation ideas

--- problem_21.py
+++ problem_21.py
@@ -5,6 +5,10 @@ Given an array of time intervals (start, end) for classroom lectures
 For example, given [(30, 75), (0, 50), (60, 150)], you should return 2.
 """
 
+#
+# Idea - http://blog.gainlo.co/index.php/2016/07/12/meeting-room-scheduling-problem/
+# Impelementation - https://github.com/r1cc4rdo/daily_coding_problem/blob/master/daily_coding_problem_21_25.py
+#
 def rooms_requried(intervals):
   starts = [(el[0],1) for el in intervals]
   ends = [(el[1],-1) for el in intervals]
@@ -19,9 +23,9 @@ def rooms_requried(intervals):
   return rooms
 
 
-assert rooms_requried([]) == 0
-assert rooms_requried([(30, 75), (0, 50), (60, 150)]) == 2
-assert rooms_requried([(30, 75), (0, 50), (10, 60), (60, 150)]) == 3
-assert rooms_requried([(60, 150)]) == 1
-assert rooms_requried([(60, 150), (150, 170)]) == 2
-assert rooms_requried([(60, 150), (60, 150), (150, 170)]) == 3
+assert rooms_required_bruteforce([]) == 0
+assert rooms_required_bruteforce([(30, 75), (0, 50), (60, 150)]) == 2
+assert rooms_required_bruteforce([(30, 75), (0, 50), (10, 60), (60, 150)]) == 3
+assert rooms_required_bruteforce([(60, 150)]) == 1
+assert rooms_required_bruteforce([(60, 150), (150, 170)]) == 2
+assert rooms_required_bruteforce([(60, 150), (60, 150), (150, 170)]) == 3
